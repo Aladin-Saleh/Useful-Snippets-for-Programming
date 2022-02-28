e82f71471cd25630ed55317bff9a76f568d31856
Aladin <67257097+Aladin-Saleh@users.noreply.github.com>
2022-02-28 12:36:33 +0100

    Create MouseControl.java

---
+++ JAVA/MouseControl.java
@@ -0,0 +1,45 @@
+import java.awt.event.*;
+
+
+public class MouseControl implements MouseListener {
+    
+
+    public MouseControl(){
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("X : "+e.getX());
+        System.out.println("Y : "+e.getY());
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+
+}
