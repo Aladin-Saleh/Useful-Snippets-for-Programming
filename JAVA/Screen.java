06cb4e19386e37c0f2d91fbd013346dfa3818761
Aladin <67257097+Aladin-Saleh@users.noreply.github.com>
2022-02-28 12:36:05 +0100

    Create Screen.java

---
+++ JAVA/Screen.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class Screen extends JFrame {
+    
+
+    public Screen(){
+        this.setLocation(0, 0);
+        this.setSize(900,900);
+        this.setResizable(false);
+        this.setTitle("");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+
+
+        this.setVisible(true);
+    }
+
+
+}
